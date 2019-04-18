db0db2d6f4fd3f0b814739c13d96d69608611442
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-17 23:43:42 -0300

    Configurar express para utilizar o Redis como data store

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const redis = require('redis')
 
 const app = express()
+const redis = redis.createClient()
+
+redis.set('visitas', 0)
 
 app.get('/', (req, res) => {
-  res.send('Home')
+  redis.get('visitas', (err, visitas) => {
+    res.send(`Visitas: ${visitas}`)
+    redis.set('visitas', parseInt(visitas) + 1)
+  })
 })
 
 app.listen(8080, () => {
