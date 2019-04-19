6a570c97011b9a7ca752f47700bfc7390d00f719
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-19 13:31:21 -0300

    Refatorar.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const redis = require('redis')
 
 const app = express()
 const redisClient = redis.createClient({
-  host: 'redis' || process.env.REDIS_URL,
+  host: 'redis://redis:6379' || process.env.REDIS_URL,
   port: 6379
 })
 
